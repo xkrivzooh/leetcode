b1d0f57f8a0f3017c58c562737f6f2f14a81ebc2
xkrivzooh <xkrivzooh@gmail.com>
2020-06-19 00:44:10 +0800

    增加了非递归解法的反转链表

---
+++ src/main/java/xyz/xkrivzooh/leetcode/Leetcode206.java
@@ -0,0 +1,32 @@
+package xyz.xkrivzooh.leetcode;
+
+/**
+ *   [206] 反转链表
+ *   https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ */
+
+
+public class Leetcode206 {
+	public static class ListNode {
+		int val;
+
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode reverseList(ListNode head) {
+		ListNode prev = null;
+		ListNode cur = head;
+		while (cur != null) {
+			ListNode temp = cur.next;
+			cur.next = prev;
+			prev = cur;
+			cur = temp;
+		}
+		return prev;
+	}
+}
