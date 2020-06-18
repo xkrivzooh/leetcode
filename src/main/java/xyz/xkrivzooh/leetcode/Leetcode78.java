a3ce85926a1b967cec11ff69789a3d935dfc751e
xkrivzooh <xkrivzooh@gmail.com>
2020-06-19 00:10:10 +0800

    add solution for LeetCode78

---
+++ src/main/java/xyz/xkrivzooh/leetcode/Leetcode78.java
@@ -0,0 +1,27 @@
+package xyz.xkrivzooh.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCode78 https://leetcode-cn.com/problems/subsets/
+ * 解法思路，逐个枚举，空集的幂集只有空集，每增加一个元素，让之前幂集中的每个集合，追加这个元素，就是新增的子集。
+ */
+public class Leetcode78 {
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		//添加空集合
+		result.add(new ArrayList<>());
+		for (int element : nums) {
+			//此处需要注意for循环中动态add元素
+			int resultSize = result.size();
+			for (int i = 0; i < resultSize; i++) {
+				List<Integer> newItem = new ArrayList<>(result.get(i));
+				newItem.add(element);
+				result.add(newItem);
+			}
+		}
+		return result;
+	}
+}
+
